f205200736b1d422818a07e1ca33b6c8af3e894e
Annie Truex <artruex@gmail.com>
2019-06-02 01:28:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song 
  
- (song_name,artist,genre)
+ attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+   def initialize(song_name,artist,genre)
     @name = song_name
     @artist = artist
     @genre = genre
